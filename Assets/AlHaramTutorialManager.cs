14000b9e75467d23cf3dcaba3127535ae17e1414
whoisthatJ <Marat.J.Urmanov@gmail.com>
2023-09-09 00:23:55 +0600

    Implemented Tutorials

---
+++ Assets/AlHaramTutorialManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlHaramTutorialManager : MonoBehaviour
+{
+    [SerializeField] private GameObject infoWindow;
+    [SerializeField] private ArrowBehaviour arrows;
+    [SerializeField] private Transform kaabaTrigger;
+    [SerializeField] private Transform[] aroundKaabaTriggers;
+
+    [SerializeField] private Transform safaTrigger;
+    [SerializeField] private Transform mauraTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrows.SetTarget(kaabaTrigger);
+    }
+    public void KaabaReached()
+    {
+        Debug.Log("KaabaReached");
+        kaabaTrigger.gameObject.SetActive(false);
+        AroundKaabaStart();
+    }
+    void AroundKaabaStart()
+    {
+        aroundKaabaTriggers[0].gameObject.SetActive(true);
+        arrows.SetTarget(aroundKaabaTriggers[0]);
+    }
+    int aroundKaabaCount;
+    public void AroundKaabaReached()
+    {
+        aroundKaabaTriggers[aroundKaabaCount % 4].gameObject.SetActive(false);
+        aroundKaabaCount++;
+        if (aroundKaabaCount >= 28)
+        {
+            infoWindow.SetActive(true);
+            arrows.PlaceObjectNearPlayer(infoWindow.transform);
+            SafaStart();
+        }
+        else
+        {
+            aroundKaabaTriggers[aroundKaabaCount % 4].gameObject.SetActive(true);
+            arrows.SetTarget(aroundKaabaTriggers[aroundKaabaCount % 4]);
+        }
+    }
+    void SafaStart()
+    {
+        safaTrigger.gameObject.SetActive(true);
+        arrows.SetTarget(safaTrigger);
+    }
+    int mauraSafaCount;
+    public void SafaReached()
+    {
+        if (mauraSafaCount == 0)
+        {
+            safaTrigger.gameObject.SetActive(false);
+            MauraSafaStart();
+        }
+        else
+        {
+            MauraSafaReached();
+        }
+    }
+    void MauraSafaStart()
+    {
+        mauraTrigger.gameObject.SetActive(true);
+        arrows.SetTarget(mauraTrigger);
+    }
+    public void MauraSafaReached()
+    {
+        mauraSafaCount++;
+        if (mauraSafaCount >= 14)
+        {
+        }
+        else
+        {
+            if (mauraSafaCount % 2 > 0)
+            {
+                mauraTrigger.gameObject.SetActive(false);
+                safaTrigger.gameObject.SetActive(true);
+                arrows.SetTarget(safaTrigger);
+            }
+            else
+            {
+                safaTrigger.gameObject.SetActive(false);
+                mauraTrigger.gameObject.SetActive(true);
+                arrows.SetTarget(mauraTrigger);
+            }
+        }
+    }
+}
--- Assets/Scripts/ArrowBehaviour.cs
+++ Assets/Scripts/ArrowBehaviour.cs
@@ -6,35 +6,43 @@ public class ArrowBehaviour : MonoBehaviour
 {
     private Transform _target;
     [SerializeField] private NavMeshAgent[] _arrows;
-    private Transform _player;
+    [SerializeField] private Transform _player;
 
     [SerializeField] private float _offset = 4f;
     [SerializeField] private float _stepDelay = 1f;
+    [SerializeField] private float _bunchDelay = 15f;
     [SerializeField] private float _arrowSpeed = 1f;
-
+    private bool reachedTarget;
     private void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player").transform;
-        SetTarget(GameObject.Find("Sphere").transform);
+        
+    }    
+    public void PlaceObjectNearPlayer(Transform obj)
+    {
+        obj.position = _player.position + _player.forward * _offset * 0.3f + Vector3.up;
     }
     public void SetTarget(Transform target)
     {
         _target = target;
-        foreach (NavMeshAgent n in _arrows)
-        {
-            n.SetDestination(_target.position);            
-        }
         PositionArrowsInSequence();
     }
     private void PositionArrowsInSequence()
     {
+        StopAllCoroutines();
         StartCoroutine(PlaceArrows());
+        StartCoroutine(UpdateArrowsInSequence());
     }
+
     private IEnumerator PlaceArrows()
     {
+        foreach (NavMeshAgent n in _arrows)
+        {
+            n.gameObject.SetActive(false);
+        }
         _arrows[0].gameObject.SetActive(true);
         _arrows[0].transform.position = _player.position + _player.forward * _offset;
         _arrows[0].speed = _arrowSpeed;
+        _arrows[0].SetDestination(_target.position);
         yield return new WaitForSeconds(_stepDelay);
         _arrows[0].speed = 0f;
         
@@ -43,8 +51,18 @@ public class ArrowBehaviour : MonoBehaviour
             _arrows[i].gameObject.SetActive(true);
             _arrows[i].transform.position = _arrows[i - 1].transform.position + _arrows[i - 1].transform.forward * _offset;
             _arrows[i].speed = _arrowSpeed;
+            _arrows[i].SetDestination(_target.position);
             yield return new WaitForSeconds(_stepDelay);
             _arrows[i].speed = 0f;
+        }
+    }
+    IEnumerator UpdateArrowsInSequence()
+    {
+        yield return new WaitForSeconds(_bunchDelay);
+        while (!reachedTarget)
+        {
+            StartCoroutine(PlaceArrows());
+            yield return new WaitForSeconds(_bunchDelay);
         }
     }
     private void PositionArrowsAroundPlayer()
